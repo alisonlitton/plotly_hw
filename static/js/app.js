88dde73bcc2b62c3387b1cfef90ec9c4f3d77982
alisonlitton <63117075+alisonlitton@users.noreply.github.com>
2020-09-17 19:42:25 -0700

    starting OTU bar chart from sample data

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,30 @@
-// Use the D3 library to read in samples.json
-function init() {
-  // select dropdown menu 
-  var dropdown = d3.select("#selDataset");
+function getPlots(id) {
+  //read the samples.json
+  d3.json("samples.json").then (sampledata=>{
+    console.log(sampledata)
+    var ids = sampledata.samples[0].otu_ids;
+    console.log(ids)
+    var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
+    console.log(sampleValues)
+    var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+    console.log(labels)
+  //narrow down the top 10 values for the plot and reverse it 
+    var OTU_top_10 = (sampledata.samples[0].otu_ids.slice(0,10)).reverse();
+    var OTU_id = OTU_top_10.map(d => "OTU" + d);
+    console.log('OTU IDS: ${OTU_id}')
+  //get the top 10 labels for the OTU plots
+    var labels = sampledata.samples[0].otu_labels.slice(0,10);
+    console.log('OTU_labels: ${labels}')
+    var trace = {
+      x: sampleValues,
+      y: OTU_id,
+      text: labels,
+      marker: {color: ''},
+      type: "bar",
+      orientation: "h",
+    };
 
-  // read the samples.json 
-  d3.json("samples.json").then((data)=> {
-      console.log(data)
+    
+  })
+};
 
-      // get the id data to the dropdwown menu
-      data.names.forEach(function(name) {
-          dropdown.append("option").text(name).property("value");
-      });
-
-      // call the functions to display the data and the plots to the page
-      getPlots(data.names[0]);
-      getDemoInfo(data.names[0]);
-  });
-}
-
-init();
